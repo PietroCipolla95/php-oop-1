483fdfa48fe80cf4d1641ed24c16a76a0bedeb35
PietroCipolla95 <pietro.cipolla.95@gmail.com>
2023-10-19 16:20:03 +0200

    add functions bootstrap and print first movie

--- index.php
+++ index.php
@@ -16,10 +16,29 @@ class Movie
         $this->genre = $genre;
         $this->director = $director;
     }
+
+    function getName()
+    {
+        return $this->name;
+    }
+
+    function getDuration()
+    {
+        return $this->duration;
+    }
+
+    function getGenre()
+    {
+        return $this->genre;
+    }
+
+    function getDirector()
+    {
+        return $this->director;
+    }
 }
 
-
-
+$matrix = new Movie('Matrix', 120, 'Sci-fi', 'Sorelle Wachowski');
 
 ?>
 
@@ -30,9 +49,43 @@ class Movie
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Movies</title>
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
 </head>
 
-<body>
+<body class="bg-black">
+
+
+    <div class="container">
+        <div class="row mt-5">
+            <div class="col-6">
+                <div class="card py-4 text-center">
+                    <h2>
+                        <?php echo $matrix->getName() ?>
+                    </h2>
+                    <p>
+                        <?php echo $matrix->getDuration() ?>
+                    </p>
+                    <p>
+                        <?php echo $matrix->getGenre() ?>
+                    </p>
+                    <p>
+                        <?php echo $matrix->getDirector() ?>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
 </body>
 
